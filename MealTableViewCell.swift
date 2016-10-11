f5a8274d8360f1c9c160234832143fe0663ca65d
James <jedwards@usagb.net>
2016-10-11 17:55:12 -0400

    First commit

---
+++ MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by James Edwards on 12/23/15.
+//  Copyright © 2015 James Edwards. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
